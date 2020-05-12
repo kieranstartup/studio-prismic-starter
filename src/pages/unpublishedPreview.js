477ec674540a4e5e487a7b64645c46a695227337
kieranstartup <info@kieranstartup.co.uk>
2020-05-12 14:05:53 +0100

    updating preview pages

---
+++ src/pages/unpublishedPreview.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { PageTemplate } from "src/templates/page";
+
+export const UnpublishedPage = props => {
+  // const previewData = window.__PRISMIC_PREVIEW_DATA__
+  // => Perform any logic from previewData to determine the correct page or template component to use.
+
+  // return <PageTemplate {...props} />;
+  return <h1>Unpublished Preview</h1>;
+};
+
+export default UnpublishedPage;
